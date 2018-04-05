c9f433e91d1bfe0d6ea020d48c7fe56383ce635f
Naomi Carrillo Plasencia <alu0100829914@ull.edu.es>
2018-04-05 22:17:26 +0100

    Añadida página de asignaturas con cards

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Academia Lagunensis' });
 });
 
+router.get('/asignaturas', function(req, res, next) {
+    res.render('asignaturas', { title: 'Asignaturas' });
+});
+
 module.exports = router;
