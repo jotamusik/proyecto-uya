4fb36bb7b7ca363e560bd5ba8c8e80666b01a7df
Naomi Carrillo Plasencia <alu0100829914@ull.edu.es>
2018-04-14 20:54:16 +0100

    Añadidos Landmark Roles de WAI-ARIA

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var Asignatura = require('../models/asignatura');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +9,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/asignaturas', function(req, res, next) {
-    res.render('asignaturas', { title: 'Asignaturas' });
+    //res.render('asignaturas', { title: 'Asignaturas' });
+    Asignatura.find({}, function (err, asignaturas) {
+
+        if(err)
+            res.render("error");
+        else
+            res.render('asignaturas', {title: 'Asignaturas', asignaturas:asignaturas});
+    });
 });
 
 module.exports = router;
